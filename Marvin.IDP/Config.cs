f8e81c3457b79088750b6b9c3599df520f8fc75f
C. M. <cristian.marin@linearsystem.it>
2024-06-10 15:39:42 +0200

    Configuring IdentityServer to Log in with The Authorization Code Flow

--- Marvin.IDP/Config.cs
+++ Marvin.IDP/Config.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace Marvin.IDP;
 
@@ -21,5 +22,31 @@ public static class Config
 
     public static IEnumerable<Client> Clients =>
         new Client[] 
-            { };
+            { 
+                new Client()
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,                    // we work with code flow so we use code
+                    // the code is delivered to the browser via URI redirection
+                    // we add a valid URI where this client is allowed to receive tokens on
+                    // the URI is the host address of our "ImageGallery.Client" web application followe by "signin-oidc"
+                    RedirectUris =
+                    {
+                        "https://localhost:7192/signin-oidc"                // See project: "ImageGallery.Client" 
+                    },
+                    // we configure which scopes are allowed for our client
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    // we need to configure a client secret for client authentication to allow client application to execute an authenticated call to the token endpoint
+                    // we give a secret, the value is ""<secret and we hash it so it can safely be stored 
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+            };
 }
